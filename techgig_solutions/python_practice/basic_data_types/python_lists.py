fbdaa574147fd422a49e4f4a3fa5184239d32afd
soluspero <soluspero@gmail.com>
2019-02-13 19:50:35 +0530

    MOD: python_list.py

--- techgig_solutions/python_practice/basic_data_types/python_lists.py
+++ techgig_solutions/python_practice/basic_data_types/python_lists.py
@@ -1,18 +1,20 @@
 ''' Read input from STDIN. Print your output to STDOUT '''
     #Use input() to read input from STDIN and use print to write your output to STDOUT
 import sys
+
 def main():
 
  # Write code here 
  Q = int(input())
  ll =[]
- for _ in range(Q):
-     qlines =input().split()
-     if int(qlines[0]) == 1:
-         for i in qlines[1:]:
-             ll.append(i)
-     elif int(qlines[0]) == 2:
-         sys.stdout.write(str(ll)+"\n")
+ strr = ""
+ for x in range(Q):
+     qlines =list(map(str,input().rstrip().split()))
+     if qlines[0] == '1':
+             ll.append(qlines[1])
+     elif qlines[0] == '2':
+         strr+= str(ll)+"\n"
+ print(strr.strip(),end="")
 
 main()
 

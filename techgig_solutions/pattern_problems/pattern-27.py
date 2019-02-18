af7bc75a9b90ca55dc85c55793c6c5555cc0579d
soluspero <soluspero@gmail.com>
2019-02-19 04:05:59 +0530

    pattern 27

---
+++ techgig_solutions/pattern_problems/pattern-27.py
@@ -0,0 +1,26 @@
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+
+"""
+        A
+      B B
+    C C C
+  D D D D
+E E E E E
+"""
+
+from sys import stdout
+def main():
+
+ # Write code here 
+  n = int(input().strip())
+  for i in range(1,n+1):
+     stdout.write(" "*(((2*n)-(i*2))))
+     s = (chr(65+i-1)+" ")*i
+     stdout.write(s.strip())
+     
+     if i < n:
+         stdout.write("\n")
+  # print()
+main()
+

64684c3779788d3d851182fb5aad4e9b0da1307d
soluspero <soluspero@gmail.com>
2019-02-19 13:38:19 +0530

    pattern-12

---
+++ techgig_solutions/pattern_problems/pattern-12.py
@@ -0,0 +1,27 @@
+''' Read input from STDIN. Print your output to STDOUT '''
+    #Use input() to read input from STDIN and use print to write your output to STDOUT
+"""
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+"""
+
+from sys import stdout
+def main():
+
+ # Write code here 
+  n = int(input().strip())
+  s=""
+  for i in range(1,n+1):
+     for j in range(1,i+1):
+         s+= str(j)+" "
+     stdout.write(s.strip())
+     
+     if i < n:
+         s=""
+         stdout.write("\n")
+  # print()
+main()
+

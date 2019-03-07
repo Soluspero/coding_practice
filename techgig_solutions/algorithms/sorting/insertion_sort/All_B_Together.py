3ef6f1aeaf580dab31ecd7bd841990a8fa9a9d39
soluspero <soluspero@gmail.com>
2019-03-07 20:15:54 +0530

    All B together

--- techgig_solutions/algorithms/sorting/insertion_sort/All_B_Together.py
+++ techgig_solutions/algorithms/sorting/insertion_sort/All_B_Together.py
@@ -23,12 +23,21 @@ bbaaacada
 ''' Read input from STDIN. Print your output to STDOUT '''
     #Use input() to read input from STDIN and use print to write your output to STDOUT
 
-from sys import stdout
+from sys import stdout 
 def main():
+    splist =  input().strip()
 
- # Write code here 
- splist = input().strip().split()
+    all_b=[]
+    none_b=[]
 
+    for i in splist:
+        if i == 'b' or i == 'B':
+            all_b.append(i)
+        else:
+            none_b.append(i)
+    
+    splist = all_b+ none_b
+
+    stdout.write("".join(splist))
 
 main()
-
